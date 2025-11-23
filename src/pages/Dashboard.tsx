e6bb194ec8254bc9be564fe6b831016d74f106cd
fiona xu <fionaxu.99@gmail.com>
2025-11-22 22:13:26 -0500

    chaticon on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -88,9 +88,9 @@ const Dashboard = () => {
             </button>
             <button 
               onClick={() => setChatOpen(true)}
-              className="w-16 h-16 rounded-full bg-primary-accent/75 flex items-center justify-center hover:scale-110 transition-all duration-300"
+              className="w-16 h-16 rounded-full bg-foreground/10 flex items-center justify-center hover:scale-110 transition-all duration-300"
             >
-              <ChatIcon className="w-9 h-9 text-primary-text" />
+              <ChatIcon className="w-9 h-9 text-foreground" />
             </button>
             <button
               onClick={() => setSettingsOpen(true)}
